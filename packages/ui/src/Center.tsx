55ed5660428b4fe14bea536f2f0d58198452c7d9
sanemi07 <arjuntripathi1724@gmail.com>
2026-03-14 19:14:46 +0530

    feat(ui): refactor Card component and add new Center, Select, and TextInput components

---
+++ packages/ui/src/Center.tsx
@@ -0,0 +1,11 @@
+
+const Center = ({children}:{children:React.ReactNode}) => {
+  return (
+    <div className="flex justify-center flex-col h-full">
+    <div className="flex justify-center">
+      {children}
+    </div></div>
+  )
+}
+
+export default Center
